0119625113bab05e84bd86aa692d0d85bb8901fd
rafajlpz <frilo_94@hotmail.com>
2022-02-08 10:35:09 +0100

    V. Pendiente 81

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form>
+    <input
+      type="text"
+      class="form-control my-2"
+      placeholder="Ingrese nombre"
+      v-model="especie.nombre"
+    />
+    <div class="form-check form-check-inlin">
+      <input
+        type="checkbox"
+        id="check-1"
+        class="form-check-input"
+        v-model="especie.categorias"
+        value="Roble"
+      />
+      <label for="check-1" class="form-check-label"> Especie 1 </label>
+    </div>
+    <div class="form-check form-check-inlin">
+      <input
+        type="checkbox"
+        id="check-2"
+        class="form-check-input"
+        v-model="especie.categorias"
+        value="Pino"
+      />
+      <label for="check-2" class="form-check-label"> Especie 2 </label>
+    </div>
+  </form>
+  <hr />
+  <p>
+    {{ especie }}
+  </p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      especie: {
+        nombre: "",
+        categorias: [],
+      },
+    };
+  },
+};
 </script>
